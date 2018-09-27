dedd480fe096bbaae485429ee59ecb21e8cf031b
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-09-26 16:16:35 -1000

    rename server.js to app.js

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
     connection: {
       host: process.env.POSTGRES_HOSTNAME,
       database: process.env.POSTGRES_DB,
+      port: process.env.POSTGRES_CONTAINER_PORT,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD
     },
